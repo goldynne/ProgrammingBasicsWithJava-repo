4307e6a9669fb7b9124da1924575033dda76b29a
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2022-10-17 10:56:49 +0300

    P06ConcatenateData

---
+++ ProgrammigBasicsSoftUni/src/FirstStepsInCoding/Lab/P06ConcatenateData.java
@@ -0,0 +1,18 @@
+package FirstStepsInCoding.Lab;
+
+import java.util.Scanner;
+
+public class P06ConcatenateData {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String town = scanner.nextLine();
+
+       System.out.printf("You are %s %s, a %s-years old person from %s.", firstName, lastName, age, town);
+       // System.out.println("You are " + firstName +" " + lastName + ", a " + age + "-years old person from " + town +".");
+
+    }
+}
