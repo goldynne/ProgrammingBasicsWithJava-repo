ba549961c0c8f5f823999b735539a4feb1e37ec2
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2022-11-05 21:36:59 +0200

    P02TriangleArea

---
+++ ProgrammigBasicsSoftUni/src/FirstStepsInCoding/MoreExercises/P02TriangleArea.java
@@ -0,0 +1,13 @@
+package FirstStepsInCoding.MoreExercises;
+
+import java.util.Scanner;
+
+public class P02TriangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = Double.parseDouble(scanner.nextLine());
+        double h = Double.parseDouble(scanner.nextLine());
+        double S = a*h/2;
+        System.out.printf("%.2f", S);
+    }
+}
