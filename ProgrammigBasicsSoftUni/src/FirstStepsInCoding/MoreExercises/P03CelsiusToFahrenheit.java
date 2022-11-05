1e734ca3bee7c40fc9316ea6775f74bf09ce851c
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2022-11-05 21:54:18 +0200

    P03CelsiusToFahrenheit

---
+++ ProgrammigBasicsSoftUni/src/FirstStepsInCoding/MoreExercises/P03CelsiusToFahrenheit.java
@@ -0,0 +1,12 @@
+package FirstStepsInCoding.MoreExercises;
+
+import java.util.Scanner;
+
+public class P03CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double celsius = Double.parseDouble(scanner.nextLine());
+        double fahrenheit = (celsius*1.8) + 32;
+        System.out.printf("%.2f", fahrenheit);
+    }
+}
