036c812b695a99bd4db7dba277dac150abc7a29a
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2022-11-16 01:24:07 +0200

    P03EvenOrOdd

---
+++ ProgrammigBasicsSoftUni/src/conditionalStatements/Lab/P03EvenOrOdd.java
@@ -0,0 +1,15 @@
+package conditionalStatements.Lab;
+
+import java.util.Scanner;
+
+public class P03EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        if (a%2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
