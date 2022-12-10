00656de63f048e7656a7e9c5beb648021f636fb9
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2022-12-10 11:13:37 +0200

    P05SmallShop

---
+++ ProgrammigBasicsSoftUni/src/conditionalStatementsAdvanced/Lab/P05SmallShop.java
@@ -0,0 +1,90 @@
+package conditionalStatementsAdvanced.Lab;
+
+import java.util.Scanner;
+
+public class P05SmallShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String product = scanner.nextLine();
+        String town = scanner.nextLine();
+        double quantity = Double.parseDouble(scanner.nextLine());
+        Double amount = 0.0;
+        if (town.equals("Sofia")) {
+            if (product.equals("coffee")) {
+                amount = 0.5;
+            } else if (product.equals("water")) {
+                amount = 0.80;
+            } else if (product.equals("beer")) {
+                amount = 1.20;
+            } else if (product.equals("sweets")) {
+                amount = 1.45;
+            } else if (product.equals("peanuts")) {
+                amount = 1.60;
+            }
+        } else if (town.equals("Plovdiv")) {
+            if (product.equals("coffee")) {
+                amount = 0.40;
+            } else if (product.equals("water")) {
+                amount = 0.70;
+            } else if (product.equals("beer")) {
+                amount = 1.15;
+            } else if (product.equals("sweets")) {
+                amount = 1.30;
+            } else if (product.equals("peanuts")) {
+                amount = 1.50;
+            }
+        } else if (town.equals("Varna")) {
+            if (product.equals("coffee")) {
+                amount = 0.45;
+            } else if (product.equals("water")) {
+                amount = 0.70;
+            } else if (product.equals("beer")) {
+                amount = 1.10;
+            } else if (product.equals("sweets")) {
+                amount = 1.35;
+            } else if (product.equals("peanuts")) {
+                amount = 1.55;
+            }
+        } System.out.printf("%f", quantity * amount);
+    }
+
+
+//        if (town.equals("Sofia")) {
+//            if (product.equals("coffee")) {
+//                System.out.printf("%f", quantity * 0.5);
+//            } else if (product.equals("water")) {
+//                System.out.printf("%f", quantity * 0.80);
+//            } else if (product.equals("beer")) {
+//                System.out.printf("%f", quantity * 1.20);
+//            } else if (product.equals("sweets")) {
+//                System.out.printf("%f", quantity * 1.45);
+//            } else if (product.equals("peanuts")) {
+//                System.out.printf("%f", quantity * 1.60);
+//            }
+//        }else if (town.equals("Plovdiv")) {
+//            if (product.equals("coffee")) {
+//                System.out.printf("%f", quantity * 0.40);
+//            } else if (product.equals("water")) {
+//                System.out.printf("%f", quantity * 0.70);
+//            } else if (product.equals("beer")) {
+//                System.out.printf("%f", quantity * 1.15);
+//            } else if (product.equals("sweets")) {
+//                System.out.printf("%f", quantity * 1.30);
+//            } else if (product.equals("peanuts")) {
+//                System.out.printf("%f", quantity * 1.50);
+//            }
+//        }else if (town.equals("Varna")) {
+//            if (product.equals("coffee")) {
+//                System.out.printf("%f", quantity * 0.45);
+//            } else if (product.equals("water")) {
+//                System.out.printf("%f", quantity * 0.70);
+//            } else if (product.equals("beer")) {
+//                System.out.printf("%f", quantity * 1.10);
+//            } else if (product.equals("sweets")) {
+//                System.out.printf("%f", quantity * 1.35);
+//            } else if (product.equals("peanuts")) {
+//                System.out.printf("%f", quantity * 1.55);
+//            }
+//        }
+}
+
