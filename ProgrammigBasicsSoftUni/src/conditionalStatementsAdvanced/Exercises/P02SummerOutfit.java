a5f56ca41f73ad7ce9a251907a1c401e2c4e613f
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2022-12-28 17:10:33 +0200

    P02SummerOutfit

---
+++ ProgrammigBasicsSoftUni/src/conditionalStatementsAdvanced/Exercises/P02SummerOutfit.java
@@ -0,0 +1,48 @@
+package conditionalStatementsAdvanced.Exercises;
+
+import java.util.Scanner;
+
+public class P02SummerOutfit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int degrees = Integer.parseInt(scanner.nextLine());
+        String time = scanner.nextLine();
+        String outfit = "something";
+        String shoes = "something";
+        if (time.equals("Morning")) {
+            if (degrees >= 10 && degrees <= 18) {
+                outfit = "Sweatshirt";
+                shoes = "Sneakers";
+            } else if (degrees > 18 && degrees <= 24) {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            } else if (degrees >= 25) {
+                outfit = "T-Shirt";
+                shoes = "Sandals";
+            }
+        } else if (time.equals("Afternoon")) {
+            if (degrees >= 10 && degrees <= 18) {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            } else if (degrees > 18 && degrees <= 24) {
+                outfit = "T-Shirt";
+                shoes = "Sandals";
+            } else if (degrees >= 25) {
+                outfit = "Swim Suit";
+                shoes = "Barefoot";
+            }
+        } else if (time.equals("Evening")) {
+            if (degrees >= 10 && degrees <= 18) {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            } else if (degrees > 18 && degrees <= 24) {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            } else if (degrees >= 25) {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+        }
+        System.out.printf("It's %s degrees, get your %s and %s.", degrees, outfit, shoes);
+    }
+}
