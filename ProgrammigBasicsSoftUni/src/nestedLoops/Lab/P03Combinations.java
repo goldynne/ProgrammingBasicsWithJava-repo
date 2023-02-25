d929f929c32eec770f0d1dbb1094fa6f0cf65041
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-02-25 20:45:10 +0200

    P03Combinations

---
+++ ProgrammigBasicsSoftUni/src/nestedLoops/Lab/P03Combinations.java
@@ -0,0 +1,24 @@
+package nestedLoops.Lab;
+
+import java.util.Scanner;
+
+public class P03Combinations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int evaluations = 0;
+        //x1 + x2 + x3 = n
+        for (int x1 = 0; x1 <= number; x1++) {
+            for (int x2 = 0; x2 <= number; x2++) {
+                for (int x3 = 0; x3 <= number; x3++) {
+                    if (x1 + x2 + x3 == number){
+                        evaluations ++;
+                    }
+                }
+
+            }
+
+        }
+        System.out.println(evaluations);
+    }
+}
